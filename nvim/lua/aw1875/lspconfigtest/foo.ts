6f18ff93fa412e1f71612188182eab8290866f9a
aw1875 <aw1875@rit.edu>
2022-06-29 03:18:50 -0400

    Removed lspconfig test files

--- nvim/lua/aw1875/lspconfigtest/foo.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Sample foo function
- *
- * @param {number} a The number passed in
- * @returns {number} The number passed in plus 1
- */
-export const foo = (a: number): number => {
-    return a + 1;
-}
