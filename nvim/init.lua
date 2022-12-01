c6458af7a4df6b2ef632d1e6222d7230ed721a8c
aw1875 <aw1875@rit.edu>
2022-11-30 21:22:14 -0500

    Modified TMUX setup and moved autogroups to their own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,34 +38,6 @@ require('wolfy.config.colorscheme')
 require('wolfy.after')
 
 -------------------------------------
--- LSP Auto Formatting
+-- Load AutoGroups
 -------------------------------------
-local Format = vim.api.nvim_create_augroup("Format", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePre *", {
-	callback = function()
-		vim.lsp.buf.format()
-	end,
-	group = Format,
-})
-
--------------------------------------
--- TMUX Window Formatting
--------------------------------------
-local TMUX = vim.api.nvim_create_augroup("TMUX", { clear = true })
-local windowName = ""
-vim.api.nvim_create_autocmd("BufReadPost,FileReadPost,BufNewFile *", {
-	callback = function()
-		local process = io.popen("tmux display-message -p \"#W\"")
-		for line in process:lines() do
-			windowName = line
-		end
-		os.execute("tmux rename-window " .. vim.fn.expand("%:t"))
-	end,
-	group = TMUX,
-})
-vim.api.nvim_create_autocmd("ExitPre *", {
-	callback = function()
-		os.execute("tmux rename-window " .. windowName)
-	end,
-	group = TMUX,
-})
+require('wolfy.config.augroups')
---
+++ nvim/lua/wolfy/config/augroups.lua
@@ -0,0 +1,54 @@
+-------------------------------------
+-- LSP Auto Formatting
+-------------------------------------
+local Format = vim.api.nvim_create_augroup("Format", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre *", {
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+	group = Format,
+})
+
+-------------------------------------
+-- TMUX Window Formatting
+-------------------------------------
+local TMUX = vim.api.nvim_create_augroup("TMUX", { clear = true })
+local windowName = ""
+vim.api.nvim_create_autocmd("BufReadPost,FileReadPost,BufNewFile *", {
+	callback = function()
+		local process = io.popen("tmux display-message -p \"#S\"")
+		if (process ~= nil) then
+			local sessionName = ""
+			for line in process:lines() do
+				sessionName = line
+			end
+			process = io.popen("tmux display-message -p \"#W\"")
+			if (process ~= nil) then
+				for line in process:lines() do
+					windowName = line
+				end
+			end
+
+			if (sessionName ~= "CS Homework") then
+				os.execute("tmux rename-window " .. vim.fn.expand("%:t"))
+			else
+				local parent_folder = vim.fn.expand("%:p:h")
+				local temp = {}
+
+				for word in string.gmatch(parent_folder, "([^\\/]+)") do
+					table.insert(temp, word)
+				end
+
+				local homework_number = temp[#temp + 1 - 2]
+				os.execute("tmux rename-window '" .. homework_number .. " [" .. vim.fn.expand("%:t") .. "]'")
+			end
+		end
+	end,
+	group = TMUX,
+})
+vim.api.nvim_create_autocmd("ExitPre *", {
+	callback = function()
+		os.execute("tmux rename-window " .. windowName)
+	end,
+	group = TMUX,
+})
