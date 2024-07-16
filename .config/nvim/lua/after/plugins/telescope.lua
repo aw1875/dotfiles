d94895f656d0a9f946562919c21559c2dedd5424
Adam Wolf <adam@travefy.com>
2024-07-15 20:22:00 -0500

    chore: clean up telescope and add hidden files to grep

--- .config/nvim/lua/after/plugins/telescope.lua
+++ .config/nvim/lua/after/plugins/telescope.lua
@@ -19,20 +19,17 @@ telescope.setup({
         file_ignore_patterns = { '%.git/.*', 'node%_modules/.*', 'zig%-cache/.*' },
     },
     pickers = {
-        find_files = {
-            hidden = true
-        }
+        find_files = { hidden = true },
+        live_grep = { additional_args = { '--hidden' } },
     },
 })
 
 local map = require('utils').map
 map('n', '<leader>ff', function()
-    builtin.find_files({
-        prompt_prefix = '  '
-    })
+    builtin.find_files({ prompt_prefix = '  ' })
 end, {})
 map('n', '<leader>fg', function()
-    builtin.live_grep({ prompt_prefix = '  ' })
+    builtin.live_grep({ prompt_prefix = '  ', })
 end, {})
 map('n', '<leader>fb', function()
     builtin.buffers({ prompt_prefix = '  ' })
@@ -43,9 +40,9 @@ end, {})
 map('n', '<leader>fd', function()
     builtin.find_files({
         prompt_prefix = '  ',
-        cwd = '~/.dotfiles',
+        cwd = '~/dotfiles',
         prompt_title = 'Searching Dotfiles',
-        search_dirs = { '~/.dotfiles', 'private' },
+        search_dirs = { '~/dotfiles' },
         file_ignore_patterns = { '%.git.*' }
     })
 end, {})
