4a6a8ffb20ad8669d391575e48e3b0da82e593eb
Adam Wolf <adam@travefy.com>
2025-01-31 16:22:04 -0600

    fix setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ mkdir -p "$XDG_CONFIG_HOME"
 mkdir -p "$XDG_CONFIG_HOME/nixpkgs"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_DIR" && git submodule update --init || echo "Failed to update submodules" && exit 1
+cd "$SCRIPT_DIR" && git submodule update --init || (echo "Failed to update submodules" && exit 1)
 
 ln -sf "$SCRIPT_DIR/config.nix" "$XDG_CONFIG_HOME/nixpkgs/"
 
